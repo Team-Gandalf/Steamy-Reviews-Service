1e60d1808c5893cef89c3fce3be23c896429ccb9
joshskkim <josh.sk.kim@gmail.com>
2020-04-30 12:27:45 -0700

    Altered schema to take an array for reviews

--- db/index.js
+++ db/index.js
@@ -1,33 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true});
 
-let reviewSchema = mongoose.Schema({
+let reviewsSchema = mongoose.Schema({
   id: Number,
   name: String,
   numReviews: Number,
-  review: {
-    id: Number,
-    description: String,
-    helpful: Number,
-    funny: Number,
-    date_posted: Date,
-    language: String,
-    thread_length: Number,
-    user: {
+  reviews: [
+    {
       id: Number,
-      username: String,
-      steam_purchaser: Boolean,
-      numProducts: Number,
-      icon: String
+      description: String,
+      helpful: Number,
+      funny: Number,
+      date_posted: Date,
+      language: String,
+      thread_length: Number,
+      user: {
+        id: Number,
+        username: String,
+        steam_purchaser: Boolean,
+        numProducts: Number,
+        icon: String
+      }
     }
-  }
+  ]
 })
 
-let Review = mongoose.model('Review', reviewSchema);
+let Review = mongoose.model('Review', reviewsSchema);
 
 
 let save = review => {
+  let entry = new Review({
 
+  })
 }
 
 let find = callback => {
