d293f7d01a9222f6aa7d55fa9b1b094138ab6098
joshskkim <josh.sk.kim@gmail.com>
2020-05-01 16:49:32 -0700

    Added header for reviews

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 const Reviews = () => {
-
+  return (
+    <div className="left-col">
+      <div className="user_reviews_sub_header">
+        Most Helpful Reviews
+        <span className="user_reviews_most_helpful_days">
+          In the past 30 days
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
