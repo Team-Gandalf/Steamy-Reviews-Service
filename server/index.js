e114e0cfeb06b5710a6cb1d146319090f560b6b4
joshskkim <josh.sk.kim@gmail.com>
2020-05-04 11:16:50 -0700

    Fixed query input

--- server/index.js
+++ server/index.js
@@ -7,13 +7,12 @@ const bodyParser = require('body-parser');
 const db = require('../db/index.js');
 
 app.use(express.static(`${__dirname}/../client/dist`));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
 app.get('/api/reviews/:search', (req, res) => {
-  const { game } = req.params.search;
-  db.find(game, (err, results) => {
+  db.find(req.params.search, (err, results) => {
     if (err) {
       res.status(400).send(err);
     } else {
