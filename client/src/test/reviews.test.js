f5004c5ce24b7125b855f1292f7935cc16f64b4b
joshskkim <josh.sk.kim@gmail.com>
2020-05-06 11:26:27 -0700

    Imported all components and necessary set up for enzyme suite

---
+++ client/src/test/reviews.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Enzyme, { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Reviews from '../components/Reviews.jsx';
+import ReviewList from '../components/ReviewList.jsx';
+import ReviewEntry from '../components/ReviewEntry.jsx';
+import ReviewProfile from '../components/ReviewProfile.jsx';
+
+
+configure({ adapter: new Adapter() });
