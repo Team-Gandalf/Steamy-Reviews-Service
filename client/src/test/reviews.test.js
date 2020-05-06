daa821429ec4101b6fa439dc9ee9c2cf99f0b1bb
joshskkim <josh.sk.kim@gmail.com>
2020-05-06 16:25:02 -0700

    Added in example data

--- client/src/test/reviews.test.js
+++ client/src/test/reviews.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Enzyme, {
-  configure, shallow, mount, render,
+  configure, shallow, mount,
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Reviews from '../components/Reviews';
@@ -8,13 +8,18 @@ import ReviewList from '../components/ReviewList';
 import ReviewEntry from '../components/ReviewEntry';
 import ReviewProfile from '../components/ReviewProfile';
 
+import exampleData from './reviews.example';
+
 
 configure({ adapter: new Adapter() });
 
-describe('Reviews', () => {
-  test('should render correctly in "debug" mode', () => {
-    const component = shallow(<Reviews debug />);
-
-    expect(component).toMatchSnapshot();
+describe('Renders components successfully', () => {
+  test('should render the Reviews component', () => {
+    expect(shallow(<Reviews />).find('.left-col').exists()).toBe(true);
   });
+
+  test('loads get request', () => {
+    const app = mount(<Reviews />);
+  });
+
 });
