98cb2092808f982036140a644e5e74b602e92c02
joshskkim <josh.sk.kim@gmail.com>
2020-05-06 17:33:12 -0700

    Testing for front-end components

--- client/src/test/reviews.test.js
+++ client/src/test/reviews.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Enzyme, {
-  configure, shallow, mount,
+  shallow,
+  mount,
 } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import Reviews from '../components/Reviews';
 import ReviewList from '../components/ReviewList';
 import ReviewEntry from '../components/ReviewEntry';
@@ -10,21 +10,16 @@ import ReviewProfile from '../components/ReviewProfile';
 
 import exampleData from './reviews.example';
 
-
-configure({ adapter: new Adapter() });
-
 describe('Renders components successfully', () => {
   test('should render the Reviews component', () => {
     expect(shallow(<Reviews />).find('.left-col').exists()).toBe(true);
   });
 
   test('loads ReviewList component', () => {
-    mount(<Reviews />);
     expect(shallow(<ReviewList allReviews={exampleData} />).exists()).toBe(true);
   });
 
   test('loads ReviewEntry component', () => {
-    mount(<ReviewList allReviews={exampleData} />);
     expect(shallow(<ReviewEntry review={exampleData[0]} />).exists()).toBe(true);
   });
 
