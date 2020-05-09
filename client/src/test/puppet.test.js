da661cb1930b7d6a808f9db6f6255ed9bae209bf
joshskkim <josh.sk.kim@gmail.com>
2020-05-09 11:55:37 -0700

    Preliminary test

---
+++ client/src/test/puppet.test.js
@@ -0,0 +1,12 @@
+require('regenerator-runtime/runtime');
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should display "google" text on page', async () => {
+    const text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain('google');
+  });
+});
