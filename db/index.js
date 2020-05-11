1459915519cd90d42aa3b5a141e9c8575f152ab8
joshskkim <josh.sk.kim@gmail.com>
2020-05-11 12:57:58 -0700

    Changed db collection name for proxy

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb://localhost/steamy',
+  'mongodb://localhost/steamyReviews',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
