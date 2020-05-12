df7f4105e264fbdfb5b0f9c50bf0d0678406673c
joshskkim <josh.sk.kim@gmail.com>
2020-05-12 11:58:02 -0700

    Created option to filter by review rating

--- client/src/components/ReviewFilter.jsx
+++ client/src/components/ReviewFilter.jsx
@@ -7,38 +7,41 @@ const ReviewFilter = (props) => {
   const { review, changeView } = props;
 
   const handleChange = (e) => {
-    console.log(e.target);
+    const view = e.target.value;
+    changeView(view);
   };
 
   if (review === null) {
     return (
-      <span className="empty"></span>
+      <span className="empty" />
     );
   }
 
   return (
     <div id="reviews_filter_options">
-      <div className="user_reviews_filter_menu">
-        <div className="title">
-          Review Type
-        </div>
-        <div className="user_reviews_filter_menu_flyout">
-          <div className="user_reviews_filter_menu_content">
-            <input type="radio" name="review_type" value="all" id="review_type_all" onChange={handleChange} />
-            <label htmlFor="review_type_all">
-              {`All (${review.game_reviews})`}
-            </label>
-            <br />
-            <input type="radio" name="review_type" value="positive" id="review_type_positive" onChange={handleChange} />
-            <label htmlFor="review_type_positive">
-              {`Positive (${review.rating})`}
-            </label>
-            <br />
-            <input type="radio" name="review_type" value="negative" id="review_type_negative" onChange={handleChange} />
-            <label htmlFor="review_type_negative">
-              {`Negative (${review.game_reviews - review.rating})`}
-            </label>
-            <br />
+      <div className="user_reviews_filter_menu flyout">
+        <div className="review_type">
+          <div className="title">
+            Review Type
+          </div>
+          <div className="user_reviews_filter_menu_flyout">
+            <div className="user_reviews_filter_menu_content">
+              <input type="radio" name="review_type" value="all" id="review_type_all" onChange={handleChange} />
+              <label htmlFor="review_type_all">
+                {`All (${review.game_reviews})`}
+              </label>
+              <br />
+              <input type="radio" name="review_type" value="positive" id="review_type_positive" onChange={handleChange} />
+              <label htmlFor="review_type_positive">
+                {`Positive (${review.rating})`}
+              </label>
+              <br />
+              <input type="radio" name="review_type" value="negative" id="review_type_negative" onChange={handleChange} />
+              <label htmlFor="review_type_negative">
+                {`Negative (${review.game_reviews - review.rating})`}
+              </label>
+              <br />
+            </div>
           </div>
         </div>
       </div>
